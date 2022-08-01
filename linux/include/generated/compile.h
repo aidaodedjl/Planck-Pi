cfeb297fd7563442099323a903a4a20e4df6f2f9
916BGAI <916772719@qq.com>
2022-08-01 15:43:21 +0800

    second commit

---
+++ linux/include/generated/compile.h
@@ -0,0 +1,6 @@
+/* This file is auto generated, version 7 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#7 Mon Aug 1 15:35:26 CST 2022"
+#define LINUX_COMPILE_BY "kjlsai"
+#define LINUX_COMPILE_HOST "Arch"
+#define LINUX_COMPILER "gcc version 11.3.0 (Buildroot 2022.05)"
---
+++ uboot/include/generated/timestamp_autogenerated.h
@@ -0,0 +1,5 @@
+#define U_BOOT_DATE "Aug 01 2022"
+#define U_BOOT_TIME "14:56:10"
+#define U_BOOT_TZ "+0800"
+#define U_BOOT_DMI_DATE "08/01/2022"
+#define U_BOOT_BUILD_DATE 0x20220801
